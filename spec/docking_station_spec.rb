35fc6db7232d16fdc10272c35e1f521ad5e4c338
Lucien Najev <luciennajev@hotmail.com>
2018-08-08 21:01:06 +0100

    step 10 done now we check that we receive a working bike from release_bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,4 +3,14 @@ require 'docking_station'
 
 describe DockingStation do
     it { is_expected.to respond_to :release_bike }
+
+
+
+    it "do we get a bike instance from release_bike and is bike.working? true" do
+      docking_station = DockingStation.new
+      bike = docking_station.release_bike
+      expect(bike.working?).to eq(true)
+    end
+
+
 end
