d9e12153c9fd32b7b536090f5c0adcb2d3087a08
Lucien Najev <luciennajev@hotmail.com>
2018-08-11 20:01:26 +0100

    refactoring

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,19 +1,23 @@
 require 'docking_station'
 
-
-
 describe DockingStation do
-
     let(:mockBike) { double :bike, :bike => "bike" }
 
-    # Nested describe block focused on the release bike method
-    describe '#release_bike' do
+    describe "initialization" do
+      it "@bikes - returns the instance variable @bikes" do
+        subject.dock(mockBike)
+        expect(subject.bikes).to eq([mockBike])
+      end
+      it "@capacity - has a readable default capacity" do
+        expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+      end
+    end
 
+    describe '#release_bike' do
       it '.release_bike - does it create a bike object' do
         subject.dock(mockBike)
         expect(subject.release_bike).to eq mockBike
       end
-
       it '.release_bike - error when no bikes available' do
         expect { subject.release_bike }.to raise_error 'No bikes available'
       end
@@ -24,44 +28,9 @@ describe DockingStation do
       it ".dock(bike) returns a bike object in an array" do
         expect(subject.dock(mockBike)).to eq([mockBike])
       end
-
       it '.dock(bike) - error when docking station is full' do
-        subject.capacity.times {subject.dock(mockBike)}
-        expect { subject.dock(mockBike) }.to raise_error 'Docking Station Full!!!'
+        DockingStation::DEFAULT_CAPACITY.times{ subject.dock(mockBike) }
+        expect{ subject.dock(mockBike) }.to raise_error 'Docking Station Full!!!'
       end
     end
-
-    #Nested describe block for the .bike instance variable(attr_reader)
-    describe '#bike (attr_reader for instance var)' do
-      it ".bikes - returns the instance variable @bikes" do
-        subject.dock(mockBike)
-        expect(subject.bikes).to eq([mockBike])
-      end
-    end
-
-
-
-    it "has a default capacity" do
-
-      expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
-    end
-
-
-
-    describe "initialization" do
-
-      subject { DockingStation.new }
-      let(:bike) { Bike.new }
-      it 'defaults capacity' do
-        described_class::DEFAULT_CAPACITY.times do
-          subject.dock(bike)
-        end
-        expect{ subject.dock(bike) }.to raise_error 'Docking Station Full!!!'
-      end
-    end
-
-
-
-
-
 end
